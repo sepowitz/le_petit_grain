50dfd72afd35f380831e05c4ba1a4a77b7744bdf
Sebastian Odell <sebastian@sanctuary.computer>
2018-01-14 10:45:07 -0500

    fades in content on load

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,23 +1,15 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-'use strict';
+"use strict";
 
-var _test = require('./test.js');
+if (!!window.addEventListener) window.addEventListener("DOMContentLoaded", function () {
+  return execute();
+});else // MSIE
+  window.attachEvent("onload", execute);
 
-var _test2 = _interopRequireDefault(_test);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-(0, _test2.default)();
-
-},{"./test.js":2}],2:[function(require,module,exports){
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var test = function test() {
-  return console.log('hellooo');
+var execute = function execute() {
+  var content = document.getElementsByClassName('content');
+  if (!content.length) return;
+  return content[0].classList.add('js-reveal');
 };
-exports.default = test;
 
 },{}]},{},[1]);
